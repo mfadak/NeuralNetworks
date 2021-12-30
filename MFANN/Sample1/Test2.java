6c003c4a9a8c6c0f0d76d50ed279a47f5d4ffe0d
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2021-12-30 17:16:55 +0300

    Test2.java

---
+++ MFANN/Sample1/Test2.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.net.URISyntaxException;
+
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.core.transfer.TransferFunction;
+import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.nnet.learning.BackPropagation;
+import org.neuroph.nnet.learning.MomentumBackpropagation;
+import org.neuroph.util.TransferFunctionType;
+
+import mfann.*;
+
+public class Test2 {
+
+	public static void main(String[] args) throws URISyntaxException {
+		File file = new File(Program.class.getResource("Data.txt").toURI());
+		NeuralNetwork<BackPropagation> ann = new MultiLayerPerceptron(TransferFunctionType.SIGMOID,2,5,1);
+		MomentumBackpropagation mbp = new MomentumBackpropagation();
+		mbp.setLearningRate(0.5);
+		mbp.setMomentum(0.8);
+		
+		
+		DataSet dataset = DataSet.createFromFile(file.getPath(), 2, 1, ",");
+		DataSet[] ds = dataset.createTrainingAndTestSubsets(0.7, 0.3);
+		DataSet egitim = ds[0];
+		DataSet test = ds[1];
+		
+		NNetwork neuralNetwork = new NNetwork(ann, mbp, egitim, test, 1000, 0.000001); 
+		neuralNetwork.train();
+		System.out.println("Train Error: "+neuralNetwork.getTrainError());
+		System.out.println("Test Error: "+neuralNetwork.getTestError());
+		neuralNetwork.displayErrorChart();
+	}
+
+}
